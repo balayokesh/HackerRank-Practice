6d0fcd58eec2df73c9f5147a44c7f4f451b5d24f
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2021-07-26 08:31:32 -0700

    add problem description

--- Contest/CoinChange.c
+++ Contest/CoinChange.c
@@ -1,3 +1,42 @@
+/*
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+You may assume that you have an infinite number of each kind of coin.
+
+Input Format
+An array of integers which represents the coins of different denominations.
+An integer whic represents total amount of money.
+
+Constraints
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+
+Output Format
+An integer which is the minimum number of coins required to make the amount.
+Sample Input 0
+1 2 5
+11
+Sample Output 0
+3
+Explanation 0
+11 = 5 + 5 + 1
+
+Sample Input 1
+2
+3
+Sample Output 1
+-1
+Explanation 1
+-1 since amount cannot be made up by any combination
+
+Sample Input 2
+1
+0
+Sample Output 2
+0
+*/
+
 #include <stdio.h>
 int logic(void);
 int i = 0;
