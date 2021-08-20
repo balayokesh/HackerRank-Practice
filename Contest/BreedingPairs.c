ba9a67ac6e0f5d8030d9e9c3ed6ce6068f14971c
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2021-08-20 21:09:40 +0530

    added solution

--- Contest/BreedingPairs.c
+++ Contest/BreedingPairs.c
@@ -1,1 +1,42 @@
-
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int negative=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]<0){
+            negative++;
+        }
+    }
+    int pairs=0;
+    if(negative!=0){
+        printf("Invalid input");
+    }
+    else{
+        int k=0;
+        int v=0;
+        for(int i=0;i<n;i++){
+           if(arr[i]%2==0 && arr[i]>0){
+              k++;
+           }
+           else if(arr[i]>0 && arr[i]%2!=0){
+              v++;
+            }
+        } 
+        if(k==v || k<v){
+          pairs=k;
+        }
+        else{
+           pairs=v;
+        }
+        printf("%d pairs",pairs);
+        
+     }        
+     
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
